4921c2ad1ae1df438e5ca1d0f255a2649c6bbac7
Federico Bond <federicobond@gmail.com>
2014-10-01 13:44:44 -0300

    Add isAlwaysX methods to NodeType

--- src/main/java/ar/edu/itba/lang/ast/NodeType.java
+++ src/main/java/ar/edu/itba/lang/ast/NodeType.java
@@ -2,5 +2,24 @@ package ar.edu.itba.lang.ast;
 
 public enum NodeType {
     ADDNODE, ANDNODE, ARGSNODE, BLOCKNODE, CALLNODE, DIVIDENODE, FALSENODE, IFNODE,
-    LITERALNODE, MULTIPLYNODE, NEGATENODE, ORNODE, SUBSTRACTNODE, TRUENODE, WHILENODE
+    LITERALNODE, MULTIPLYNODE, NEGATENODE, ORNODE, SUBSTRACTNODE, TRUENODE, WHILENODE;
+
+    public boolean isAlwaysTrue() {
+        switch(this) {
+            case TRUENODE:
+            case LITERALNODE:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean isAlwaysFalse() {
+        switch(this) {
+            case FALSENODE:
+                return true;
+            default:
+                return false;
+        }
+    }
 }
