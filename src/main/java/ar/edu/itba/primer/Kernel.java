7c968997bce7b39fb85598ae613d5c0490c804f2
Federico Bond <federicobond@gmail.com>
2014-10-28 20:53:56 -0300

    Add more test programs

--- src/main/java/ar/edu/itba/primer/Kernel.java
+++ src/main/java/ar/edu/itba/primer/Kernel.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class Kernel {
@@ -92,6 +93,21 @@ public class Kernel {
         if (str == null) {
             return null;
         }
-        return Integer.valueOf((String)str);
+        try {
+            return Integer.valueOf((String) str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    public static Object split(Object str, Object separator) {
+        String[] aux = ((String)str).split((String)separator);
+        ArrayList result = new ArrayList();
+        Collections.addAll(result, aux);
+        return result;
+    }
+
+    public static Object concat(Object s1, Object s2) {
+        return s1.toString() + s2.toString();
     }
 }
