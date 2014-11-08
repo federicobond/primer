861cf0dcc1e0a38672d72be3541156165f805e40
Federico Bond <federicobond@gmail.com>
2014-11-08 02:23:03 -0300

    Add --help and -h options to bin/primer

--- src/main/java/ar/edu/itba/primer/Main.java
+++ src/main/java/ar/edu/itba/primer/Main.java
@@ -7,18 +7,40 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
-    public static final String EXT = "primer";
+    private static final String VERSION = "1.0";
+    private static final Map<String, String> commandHelp = new TreeMap<>();
+    static {
+        commandHelp.put("run", "executes the file");
+        commandHelp.put("compile", "compiles the file to a JVM .class");
+        commandHelp.put("ast", "prints the AST for the file");
+        commandHelp.put("bytecode", "prints a human-readable version of the JVM bytecode for the file");
+        commandHelp.put("tokens", "prints the list of tokens for the file");
+    }
 
     public static void printUsage() {
-        System.err.println("usage: primer COMMAND filename.primer");
+        System.err.println("usage: primer COMMAND programfile");
+    }
+
+    private static void printHelp() {
+        System.out.printf("primer, version %s\n", VERSION);
+        printUsage();
+
+        System.out.println();
+        for (Map.Entry<String, String> e : commandHelp.entrySet()) {
+            System.out.printf("  %-15s %s\n", e.getKey(), e.getValue());
+        }
     }
 
     public static void main(String[] args) {
+        if (Arrays.binarySearch(args, "--help") != -1
+            || Arrays.binarySearch(args, "-h") != -1) {
+            printHelp();
+            return;
+        }
         if (args.length < 2) {
             printUsage();
             System.exit(1);
