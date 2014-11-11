b886b721b64e9108d97720cb417198259557a532
Federico Bond <federicobond@gmail.com>
2014-11-11 00:42:57 -0300

    Add is_prime and tictactoe programs

--- src/main/java/ar/edu/itba/primer/Kernel.java
+++ src/main/java/ar/edu/itba/primer/Kernel.java
@@ -59,6 +59,10 @@ public class Kernel {
         return ((ArrayList)list).get((int)index);
     }
 
+    public static Object list_set(Object list, Object index, Object value) {
+        return ((ArrayList)list).set((int)index, value);
+    }
+
     public static Object list_remove(Object list, Object index) {
         return ((ArrayList)list).remove(index);
     }
